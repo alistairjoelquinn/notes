4d28d9c8e8edcbe69f09efbcb3a36740de158cb2
alistairjoelquinn <alistairjoelquinn@gmail.com>
2020-04-07 16:33:21 +0200

    basic home page formatting

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-        <View>
-
+    <View style={{
+        padding: 50
+    }}>
+        <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center'
+        }}>
+            <TextInput 
+                placeholder='Add to list...'
+                style={{
+                    width: '80%',
+                    borderColor: 'black',
+                    borderWidth: 1,
+                    padding: 10
+                }}
+            />
+            <Button title='ADD' />
         </View>
         <View>
 
